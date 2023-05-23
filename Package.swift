8cd3f2c91fbcef5026817eeacd86b892440ae45d
Kevin Tran <seanap@asleep.ai>
2023-05-24 08:32:45 +0900

    Release v0.2.0

--- Package.swift
+++ Package.swift
@@ -5,21 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "YTimeTeller",
-    
+
     platforms: [
       .macOS(.v10_15), .iOS(.v14), .tvOS(.v14)
     ],
 
     products: [
-        
         .library(
             name: "YTimeTeller",
             targets: ["YTimeTeller"]),
     ],
+
     dependencies: [
-        
     ],
+
     targets: [
-        .binaryTarget(name: "YTimeTeller", url: "https://guileless-empanada-9dab90.netlify.app/YTimeTeller.xcframework.zip", checksum: "8faec6e4ba8ef4122c1c012d2885411fa0d214c670e17b5ad002aa08d2ef0f4b")
+        .binaryTarget(
+            name: "YTimeTeller",
+            url: "https://github.com/aseanap/YTimeTellerSPM/releases/download/v0.2.0/YTimeTeller.xcframework.zip",
+            checksum: "8faec6e4ba8ef4122c1c012d2885411fa0d214c670e17b5ad002aa08d2ef0f4b"
+        )
     ]
 )
